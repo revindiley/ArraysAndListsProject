ab94a4457f37f79ddc3ddf6b3c938c6f9d58c1c6
Devin Riley <devin.riley144@gmail.com>
2023-01-06 09:42:31 -0600

    finished TODO\

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ArraysAndLists
 {
@@ -9,11 +11,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            var evens = new List<int>();
+            var odds = new List<int>(); 
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -22,11 +28,44 @@ namespace ArraysAndLists
              * or the odds List
              */
 
+            //for(int i= 0; i<numbers.Length; i++) // same as doing the foreach loop below
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        evens.Add(numbers[i]);
+            //    }
+            //    else
+            //    {
+            //        odds.Add(numbers[i]);
+            //    }
+            //}
+
+            foreach( var num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
+
+            foreach (var oddNumber in odds)
+            {
+                Console.WriteLine($"{oddNumber} is an odd number");
+            }
         }
     }
 }
